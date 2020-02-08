13ae6750576c64ad123c41ddee6fd8aa42594c31
Steven Lam <stevenvl@gmail.com>
2020-02-07 23:31:31 -0500

    finally able to combine all character sets and checked for functioality

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var charSetAZ   = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-var rndBigAZ    = charSetAZ.split();
-var rndSmallAZ  = charSetAZ.toLowerCase();
-var rndNum      = Math.floor(Math.random() * 10);
+var generateBtn   = document.querySelector("#generate");
+var charSetBig    = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var charSetSmall  =("abcdefghijklmnopqrstuvwxyz")
+var rndBigAZ      = charSetBig.split(" ");
+var rndSmallAZ    = charSetSmall.split(" ");
+var rndSym        = ("!@#$%^&*()_-+,/?").split(" ");
+var rndNum        = ("1234567890").split(" ");
 
-console.log(rndSmallAZ);
-console.log(rndBigAZ);
-console.log(rndNum);
+//Combined all string into one variable
+var allCharSet   = rndBigAZ + rndSmallAZ + rndSym + rndNum;
+
+//Attempted to get length of all Characters:
+var charSetLength = allCharSet.length;  
+
+//returned 152...?! 26 uppercase + 26 lowercase + 10 + 17 symbols !==152
+//Perhaps they are counting the ","
+//Added a 'space' between " " and my problem was fixed
+console.log(charSetLength);
+
+//Checks to ensure functionality
+console.log(allCharSet);
+// console.log(rndSym);
+// console.log(rndSmallAZ);
+// console.log(rndBigAZ);
+// console.log(rndNum);
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword ( 
 
+) {
+
+
+
+}
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword
+generateBtn.addEventListener("click", writePassword())
+
