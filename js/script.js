0b864acc25b007d4c99dfe82a634c974149a6276
Steven Lam <stevenvl@gmail.com>
2020-02-11 00:38:13 -0500

    mathfunction almost complete--something is not working correctly.attempting to establish range for mathrnd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var charSetBig = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
@@ -5,94 +6,68 @@ var charSetSmall = ("abcdefghijklmnopqrstuvwxyz");
 var rndBigAZ = charSetBig.split(" ");
 var rndSmallAZ = charSetSmall.split(" ");
 var rndSym = ("!@#$%^&*()_-+,/?").split(" ");
-var rndNum = (123456789);
+var rndNum = (1234567890);
 var criteriaArray = []
 
 var charsetArray = rndBigAZ.concat(rndSmallAZ.concat(rndSym.concat(rndNum)));
 console.log(charsetArray);
 
 // Write password to the #password input; create variable declare- console log it;
-  function writePassword() {
-    var parsedPasswordLength = parseInt(passwordLength);
-    var passwordLength = prompt("How many characters would you like your password to be?", "value must be from '8-128'")
-    if(parsedPasswordLength <8 || parsedPasswordLength >128) {
-      passwordLength
-      //if is not a number reprompt; if no value is inputted = 'null'
-    } else if (isNaN(passwordLength)) {
+function writePassword() {
+  var passwordLength = prompt("How many characters would you like your password to be?", "value must be from '8-128'")
+  var parsedPasswordLength = parseInt(passwordLength);
+  if (parsedPasswordLength < 8 || parsedPasswordLength > 128) {
+    passwordLength;
+    //if is not a number reprompt; if no value is inputted = 'null'
     writePassword();
-    } var userUpperCase =confirm("Would you like to use Upper Case Letters?");
-    if (userUpperCase) { 
-      criteriaArray.push(rndBigAZ);
-    } var userLowerCase= confirm("Would you like to use Lower Case Letters?");
-    if (userLowerCase) {
-      criteriaArray.push(rndSmallAZ);
-    } var userSym = confirm("Would you like to use Special Characters?");
-    if (userSym) {
-      criteriaArray.push(rndSym);
-    } var userNum = confirm("Would you like to use Numbers?");
-    if (userNum) {
-      criteriaArray.push(rndNum);
-    }
-    console.log(criteriaArray);
-  }  
-  //function to generate password 
-  // var password = generatePassword();
+  }
+  var userUpperCase = confirm("Would you like to use Upper Case Letters?");
+  if (userUpperCase) {
+    criteriaArray.push(rndBigAZ);
+  } var userLowerCase = confirm("Would you like to use Lower Case Letters?");
+  if (userLowerCase) {
+    criteriaArray.push(rndSmallAZ);
+  } var userSym = confirm("Would you like to use Special Characters?");
+  if (userSym) {
+    criteriaArray.push(rndSym);
+  } var userNum = confirm("Would you like to use Numbers?");
+  if (userNum) {
+    criteriaArray.push(rndNum);
+  }
+  if (criteriaArray == []) {
+    writePassword();
+  }
 
+  for (var i = 0; i < (parsedPasswordLength + 1); i++) {
+    // var generatePW = Math.floor(Math.random(Math.floor(Math.random() * criteriaArray.length))* criteriaArray[]);
+
+    // selects random number with range length of userChoices
+    var passwordIndex = Math.floor(Math.random() * criteriaArray.Length);
+
+    // attempting to use passwordIndex to select a random number the length of specified index????
+    var passwordText = Math.floor(Math.random() * criteriaArray[passwordIndex]
+    // document.getElementById("#password").innerHTML(passwordText)
+  
   
 
-    //Attempting to reprompt if password is < 8 || greater than 128 or cancel button is clicked; ternary operator =>takes 3 arguments
-    //not working...write smaller function ==> code may be running before the user has a chance to input the value;
-
-  //   if ((parsedPasswordLength) < 8 || (parsedPasswordLength > 128) || (passwordLength === false)) {
-  //     writePassword();
-  //   } else {
-  //   //storing values to push character set into criteriaArray array...
-  //   if (confirm("Would you like to include Upper Case Letters?") === true) {
-  //     criteriaArray.push(rndBigAZ);
-  //   }
-  //   if (confirm("Would you like to include Lower Case Letters?") === true) {
-  //     criteriaArray.push(rndSmallAZ);
-  //   }
-  //   if (confirm("Would you like to include Special Characters?") === true) {
-  //     criteriaArray.push(rndSym);
-  //   }
-  //   if (confirm("Would you like to include Numbers?") === true) {
-  //     criteriaArray.push(rndSym);
-  //   }
-  // }
+}
 
 
-  //Initial thoughts on how to solve problem but abandoned
-
-    // var password = generatePassword();
-    //     if (bigAZYes === false && smallAZYes === false && symYes === false && numYes === false) {
-    //       alert("You must select at least 1 criteria!");
-    //       criteria();
-    // }
-    // if (bigAZYes === true && smallAZyes === true && symYes === true && numYes === true) {
-    // password[Math.floor(Math.random() * charsetArray.length)];
-    // console.log(password);
 
 
-    // if (bigAZYes === true && smallAZyes === false && symYes === false && numYes === false) {
-    // password = Math.floor(Math.random() * 
+  //returning NaN...Why?
 
-    // if (bigAZYes === false && smallAZyes === false && symYes === true && numYes === false) {
-    //     password = Math.floor(Math.random() * rndSym.length);
+  console.log(criteriaArray);
 
-    //     if (bigAZYes === false && smallAZyes === false && symYes === false && numYes === true) {
-    //       password = Math.floor(Math.random() * rndNum.length)
-    //       }
-    //       }
-    //       console.log(password);
-    //       }
-    //     }
-    //   }
-    // }
-    // var passwordText = document.querySelector("#password");
 
-    // passwordText.value = password;
-  
+
+
+
+
+
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
