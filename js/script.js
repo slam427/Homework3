0c3a36ce4d57a7f74fb9a74924fb2e2e50c69d11
Steven Lam <stevenvl@gmail.com>
2020-02-11 22:43:09 -0500

    concat of num set & sym set rtns single num str & single sym str-attempting manual input of sym and num as vars

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ var charSetBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var charSetSmall = "abcdefghijklmnopqrstuvwxyz";
 var bigAZArray = charSetBig.split("");
 var smallAZArray = charSetSmall.split("");
-var rndSym = "!@#$%^&*()_-+,/?".split("");
-var numSet = "1234567890";
+var rndSym = "!@#$%^&*()_-+/?".split("");
+// var numSet = "1234567890";
+
+
 var rndNum = numSet.split("");
 
 rndNum+=rndSym;
@@ -30,53 +32,49 @@ function writePassword() {
     
   var parsedPasswordLength;
   // not returning prompt when 8 >input> 128...can't figure out why...
-  while(!parsedPasswordLength) {
+  //While statement does not work...why??
+  while(!parsedPasswordLength) {  
     var passwordLength = prompt("How many characters would you like your password to be?", "value must be from '8-128'");
     var parsedPasswordLength = parseInt(passwordLength);
     if (parsedPasswordLength <= 8 && parsedPasswordLength <= 128) {
       parsedPasswordLength =true;
     }
   }
-  //  (parsedPasswordLength < 8 || parsedPasswordLength > 128) 
-  //   //if is not a number reprompt; if no value is inputted = 'null'
-  //   writePassword();
-  
+//=======================================================================================================================================================================DOES NOT WORK
   var criteriaArray=[];
   var userUpperCase = confirm("Would you like to use Upper Case Letters?");
   if (userUpperCase) {
-    var criteriaArray = criteriaArray.concat(bigAZArray);
     console.log("concat check",criteriaArray.concat(bigAZArray));
   } var userLowerCase = confirm("Would you like to use Lower Case Letters?");
   if (userLowerCase) {
     var criteriaArray = criteriaArray.concat(smallAZArray);
   } var userSym = confirm("Would you like to use Special Characters?");
   if (userSym) {
+    //concatenation of rndSym returns single string of symbols
     var criteriaArray = criteriaArray.concat(rndSym);
   } var userNum = confirm("Would you like to use Numbers?");
   if (userNum) {
+    //concatenation of rndNum returns single string of numbers
     var criteriaArray = criteriaArray.concat(rndNum);
-    console.log(criteriaArray);
+    console.log("concat check",criteriaArray.concat(rndNum)); 
+    // ==============================================================================================================WORKS!!!
   }
 
-  // var password = "";
-  // for (var i = 1; i <= parsedPasswordLength; i++) {
-  //   // var generatePW = Math.floor(Math.random(Math.floor(Math.random() * criteriaArray.length))* criteriaArray[]);
+  var password = "";
+  //why is this only running once???
+  for (var i =0; i <= parsedPasswordLength; i++) {
+    // var generatePW = Math.floor(Math.random(Math.floor(Math.random() * criteriaArray.length))* criteriaArray[]);
     
-  //   // selects random number with range length of criteriaArray
-  //   var rndIndex = Math.floor(Math.random() * criteriaArray.Length);
-    
-  //   // attempting to use passwordIndex to select a random number the length of specified index????
-  //   var rndChar = criteriaArray[rndIndex]
-  //   password += rndChar
-  // }
-  // console.log(password)
- 
+    // selects random number with range length of criteriaArray
+    var rndIndex = Math.floor(Math.random() * criteriaArray.length);
   
-  
-  
-  
-  //returning NaN...Why?
-  
+    // attempting to use passwordIndex to select a random number the length of specified index????
+    var rndChar = criteriaArray[rndIndex]
+    var password =password.concat(rndChar);
+  }
+    //password generated....BUT spits out 'x'
+    console.log(password);
+
   
 }
 
